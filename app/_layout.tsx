1c1f3fd449e64f26d04e2420dba87a3022f3c969
isayu <isayu1571@gmail.com>
2026-07-12 22:31:20 +0800

    試試看

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,18 @@
-import { useFonts } from 'expo-font';
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import * as SplashScreen from 'expo-splash-screen';
+import * as SplashScreen from "expo-splash-screen";
 import React, { useEffect } from "react";
 import { AppProvider } from "../context/AppContext";
 import { ThemeProvider } from "../context/ThemeContext";
-
+//修
 // 阻止啟動畫面自動隱藏，直到字體準備好
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     // 🌟 這裡填入妳定義的名字與檔案路徑
-    'PressStart2P': require('../assets/fonts/PressStart2P-Regular.ttf'),
-    'Cubic11': require('../assets/fonts/Cubic_11.ttf'),
+    PressStart2P: require("../assets/fonts/PressStart2P-Regular.ttf"),
+    Cubic11: require("../assets/fonts/Cubic_11.ttf"),
   });
 
   useEffect(() => {
@@ -25,7 +25,7 @@ export default function RootLayout() {
   if (!loaded && !error) {
     return null; // 或者回傳一個簡單的 Loading 畫面
   }
-  
+
   return (
     <ThemeProvider>
       <AppProvider>
@@ -33,7 +33,4 @@ export default function RootLayout() {
       </AppProvider>
     </ThemeProvider>
   );
-
 }
-
-
